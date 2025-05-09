e96bf84686b6f0634febf975a2cda901ddeacc28
Dmytro Hopko <gopkodimon@gmail.com>
2025-05-09 15:49:50 +0000

    feat: renamed text handler folder

--- src/bot/handlers/messages/index.ts
+++ src/bot/handlers/messages/index.ts
@@ -1,6 +1,6 @@
 import { Bot, Context } from 'grammy';
 import { PrismaClient } from '@prisma/client';
-import { handleTextMessage } from './textMessages/index.js';
+import { handleTextMessage } from './text/index.js';
 import { handleMessageReactions } from './reactions/index.js';
 
 export const registerMessages = async (bot: Bot<Context>, db: PrismaClient) => {
